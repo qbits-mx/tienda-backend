a83b35d357c1d13b1829d227fec239d1d22f8b6c
claudiaigs <30503254+claudiaigs@users.noreply.github.com>
2021-12-06 13:47:17 -0600

    borrador de la clase mapper

--- src/main/java/mx/qbits/tienda/api/mapper/AnuncioMapper.java
+++ src/main/java/mx/qbits/tienda/api/mapper/AnuncioMapper.java
@@ -1,4 +1,45 @@
 package mx.qbits.tienda.api.mapper;
 
-public class AnuncioMapper {
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+public interface AnuncioMapper{
+	final String CAMPOS_ANUNCIO: "id, id_usuario, id_comprador, id_catalogo_condicion, id_catalogo_forma_pago, id_catalogo_zona_entrega, descripcion, vigencia, datos_contacto, validado, notificado, revisado, activo, comprado, fecha_compra, estrellas, comentario, comentario_aprobado";
+
+	@Results(id="AnuncioMapping", value = {
+					@Result(property = "id",              					column = "id"),
+					@Result(property = "id_usuario",      					column = "id_usuario"),
+					@Result(property = "id_comprador",    					column = "id_comprador"),
+					@Result(property = "id_catalogo_condicion", 		column = "id_ctalogo_condicion")
+					@Result(property = "id_catalogo_forma_pago",		column = "id_catalogo_forma_pago"),
+					@Result(property = "id_catalogo_zona_entrega",	column = "id_catalogo_zona_entrega"),
+					@Result(property = "descripcion",								column = "descripcion"),
+					@Result(property = "vigencia",									column = "vigencia"),
+					@Result(property = "datos_contacto",						column = "datos_contacto"),
+					@Result(property = "validado",									column = "validado"),
+					@Result(property = "notificado",								column = "notificado"),
+					@Result(property = "revisado",									column = "revisado"),
+					@Result(property = "activo",										column = "activo"),
+					@Result(property = "comprado",									column = "comprado"),
+					@Result(property = "fecha_compra",							column = "fecha_compra"),
+					@Result(property = "estrellas",									column = "estrellas"),
+					@Result(property = "comentario",								column = "comentario"),
+					@Result(property = "comentario_aprobado",				column = "comentario_aprobado")
+	})
+	@Insert("INSERT INTO anuncio(id_usuario, id_catalogo_condicion, id_catalogo_forma_pago, id_catalogo_zona_entrega, descripcion, vigencia, datos_contacto) VALUES(#{id_usuario}, #{id_catalogo_condicion}, #{id_catalogo_forma_pago}, #{id_catalogo_zona_entrega, #{descripcion} #{vigencia} #{datos_contacto}}")
+	int insert(int id_usuario, int id_catalogo_condicion, int id_catalogo_forma_pago, int id_catalogo_zona_entrega, String descripcion, date vigencia, String datos_contacto) throws SQLException;
 }
+
+
+
+
+}
+
