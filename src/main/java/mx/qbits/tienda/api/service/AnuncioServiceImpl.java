02657298a73524950fbfd814c2e24eb0b8152519
claudiaigs <30503254+claudiaigs@users.noreply.github.com>
2021-12-06 13:51:23 -0600

    Update AnuncioServiceImpl.java

--- src/main/java/mx/qbits/tienda/api/service/AnuncioServiceImpl.java
+++ src/main/java/mx/qbits/tienda/api/service/AnuncioServiceImpl.java
@@ -2,6 +2,21 @@ package mx.qbits.tienda.api.service;
 
 import mx.qbits.tienda.api.mapper.AnuncioMapper;
 
-public class AnuncioServiceImpl implements AnuncioService{
-    private AnuncioMapper anuncioMapper;
+public class AnuncioServiceImpl{
+	AnuncioMapper mapper = new AnuncioMapper();
+	int salvaAnuncio(Anuncio anuncio) throws Exception{
+		int usuario = anuncio.getIdUsuario();
+		int catalogoPago = anuncio.getCatalogoFormaPago();
+		int catalogoCondicion = anuncio.getCatalogoCondicion();
+		int catalogoEntrega = anuncio.getCatalogoZOnaEntrega();
+		String descripcion = anuncio.getDescripcion();
+		date vigencia = anuncio.getVigencia();
+		String datosContacto = anuncio.getDatosContacto();
+
+		try{
+			mapper.salvaAnuncio(usuario, catalogoPago, catalogoCondicion, catalogoCondicion, catalogoEntrega, descripcion, vigencia, datosContacto);
+		} catch(SQLException e){
+			throw new Exception("Error de Conexión con la base de datos");
+		}
+	}
 }
