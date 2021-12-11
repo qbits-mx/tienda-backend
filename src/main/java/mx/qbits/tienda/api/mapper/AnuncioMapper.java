89fcd37cfe074eb3e6746dff6b3a8b88baa8888c
claudiaigs <30503254+claudiaigs@users.noreply.github.com>
2021-12-11 14:23:21 -0600

    Correcciones del mapper

--- src/main/java/mx/qbits/tienda/api/mapper/AnuncioMapper.java
+++ src/main/java/mx/qbits/tienda/api/mapper/AnuncioMapper.java
@@ -16,29 +16,31 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnuncioMapper{
-	final String CAMPOS_ANUNCIO = "id, id_usuario, id_comprador, id_catalogo_condicion, id_catalogo_forma_pago, id_catalogo_zona_entrega, id_catalogo_departamento,descripcion, vigencia, datos_contacto, validado, notificado, revisado, activo, comprado, fecha_compra, estrellas, comentario, comentario_aprobado";
+	final String CAMPOS_ANUNCIO: "id, id_usuario, id_comprador, id_catalogo_departamento, id_catalogo_condicion, id_catalogo_forma_pago, id_catalogo_zona_entrega, descripcion, vigencia, datos_contacto, validado, notificado, revisado, activo, comprado, fecha_compra, estrellas, comentario, comentario_aprobado";
 
 	@Results(id="AnuncioMapping", value = {
-					@Result(property = "id",              					column = "id"),
-					@Result(property = "id_usuario",      					column = "id_usuario"),
-					@Result(property = "id_comprador",    					column = "id_comprador"),
-					@Result(property = "id_catalogo_condicion", 		column = "id_ctalogo_condicion"),
-					@Result(property = "id_catalogo_forma_pago",		column = "id_catalogo_forma_pago"),
-					@Result(property = "id_catalogo_zona_entrega",	column = "id_catalogo_zona_entrega"),
-					@Result(property = "id_catalogo_departamento",	column = "id_catalogo_departamento"),
-					@Result(property = "descripcion",								column = "descripcion"),
-					@Result(property = "vigencia",									column = "vigencia"),
-					@Result(property = "datos_contacto",						column = "datos_contacto"),
-					@Result(property = "validado",									column = "validado"),
-					@Result(property = "notificado",								column = "notificado"),
-					@Result(property = "revisado",									column = "revisado"),
-					@Result(property = "activo",										column = "activo"),
-					@Result(property = "comprado",									column = "comprado"),
-					@Result(property = "fecha_compra",							column = "fecha_compra"),
-					@Result(property = "estrellas",									column = "estrellas"),
-					@Result(property = "comentario",								column = "comentario"),
-					@Result(property = "comentario_aprobado",				column = "comentario_aprobado")
+					@Result(property = "id",              				column = "id"),
+					@Result(property = "idUsuario",      					column = "id_usuario"),
+					@Result(property = "idComprador",    					column = "id_comprador"),
+					@Result(property = "idCatalogoDepartamento",	column = "id_catalogo_departamento"),
+					@Result(property = "idCatalogoCondicion", 		column = "id_catalogo_condicion")
+					@Result(property = "idCatalogoFormaDePago",		column = "id_catalogo_forma_pago"),
+					@Result(property = "idCatalogoZonaEntrega",		column = "id_catalogo_zona_entrega"),
+					@Result(property = "descripcion",							column = "descripcion"),
+					@Result(property = "vigenciaAnuncio",					column = "vigencia"),
+					@Result(property = "contacto",								column = "datos_contacto"),
+					@Result(property = "validado",								column = "validado"),
+					@Result(property = "notificado",							column = "notificado"),
+					@Result(property = "revisado",								column = "revisado"),
+					@Result(property = "activo",									column = "activo"),
+					@Result(property = "comprado",								column = "comprado"),
+					@Result(property = "fechaCompra",							column = "fecha_compra"),
+					@Result(property = "estrellas",								column = "estrellas"),
+					@Result(property = "comentario",							column = "comentario"),
+					@Result(property = "comentarioAprobado",			column = "comentario_aprobado"),
+					@Result(property = "multimedia",	column = "mulimedia"),
 	})
 	@Insert("INSERT INTO anuncio(id_usuario, id_catalogo_condicion, id_catalogo_forma_pago, id_catalogo_zona_entrega id_catalogo_departamento, descripcion, vigencia, datos_contacto) VALUES(#{id_usuario}, #{id_catalogo_condicion}, #{id_catalogo_forma_pago}, #{id_catalogo_zona_entrega}, #{id_catalogo_departamento}, #{descripcion}, #{vigencia}, #{datos_contacto}}")
 	int insert(int id_usuario, int id_catalogo_condicion, int id_catalogo_forma_pago, int id_catalogo_zona_entrega, int catalogo_departamento, String descripcion, Date vigencia, String datos_contacto) throws SQLException;
+
 }
