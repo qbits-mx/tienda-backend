0c8aa2c50e3b8f718572d710c4a8707ab7066907
Ricardo G <30503104+Gohstreck@users.noreply.github.com>
2021-12-13 12:12:11 -0600

    Cambiando error en el mapper.

--- src/main/java/mx/qbits/tienda/api/mapper/CatalogoJCatalogoMaestroMapper.java
+++ src/main/java/mx/qbits/tienda/api/mapper/CatalogoJCatalogoMaestroMapper.java
@@ -16,16 +16,13 @@ public interface CatalogoJCatalogoMaestroMapper {
  
     final String CAMPOS = "catalogo.id, id_catalogo_categoria, tipo_catalogo, nombre, activo";
 
-    @Results(id="estandar", value = {
+    @Results(id="CatalogoJCatalogoMaestroMapping", value = {
         @Result(property = "id", column = "catalogo.id"),
         @Result(property = "idMaestro", column = "id_catalogo_categoria"),
         @Result(property = "tipoCatalogo", column = "catalogo_maestro.tipo_catalogo"),
         @Result(property = "nombre" ,column = "nombre"),
         @Result(property = "activo", column = "activo")
-    })
-
-    @ResultMap("estandar")
-    @Select("SELECT " + CAMPOS + " FROM catalogo inner join catalogo_maestro on catalogo.id_catalogo_categoria = catalogo_maestro.id")
+    })@Select("SELECT " + CAMPOS + " FROM catalogo inner join catalogo_maestro on catalogo.id_catalogo_categoria = catalogo_maestro.id")
     List<CatalogoJCatalogoMaestro> getAll() throws SQLException;
     
 
