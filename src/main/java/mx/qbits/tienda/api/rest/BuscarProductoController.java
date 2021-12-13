2a07d321e83280278930f64610b34f9ecc5fdf40
bcalvario <bcalvario@ciencias.unam.mx>
2021-12-13 06:35:33 -0600

    Add: controller de buscar producto

---
+++ src/main/java/mx/qbits/tienda/api/rest/BuscarProductoController.java
@@ -0,0 +1,28 @@
+package mx.qbits.tienda.api.rest;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import mx.qbits.tienda.api.model.domain.Anuncio;
+import mx.qbits.tienda.api.model.exceptions.BusinessException;
+import mx.qbits.tienda.api.service.BuscarProductoService;
+
+@RestController
+@RequestMapping(value = "/api")
+public class BuscarProductoController {
+
+    private BuscarProductoService buscarProductoService;
+
+    public BuscarProductoController(BuscarProductoService buscarProductoService) {
+        this.buscarProductoService = buscarProductoService;
+    }
+
+    @GetMapping(path = "/dame-datos.json", produces = "application/json; charset=utf-8")
+    public List<Anuncio> dame() throws BusinessException {
+        return buscarProductoService.dameDatos();
+    }
+}
