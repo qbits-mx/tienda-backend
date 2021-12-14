0835d53d51000f456f1e96182fd979d5a8f06992
gerardomt <gerardomt@ciencias.unam.mx>
2021-12-13 18:53:40 -0600

    actualizaCompra actuliza también fechaCompra y se agregaron mensajes de error

--- src/main/java/mx/qbits/tienda/api/service/AnuncioServiceImpl.java
+++ src/main/java/mx/qbits/tienda/api/service/AnuncioServiceImpl.java
@@ -1,54 +1,70 @@
 package mx.qbits.tienda.api.service;
 
+import java.sql.Date;
 import java.sql.SQLException;
+
 import org.springframework.stereotype.Service;
+
 import mx.qbits.tienda.api.mapper.AnuncioMapper;
 import mx.qbits.tienda.api.model.domain.Anuncio;
+import mx.qbits.tienda.api.model.enumerations.EnumMessage;
 import mx.qbits.tienda.api.model.exceptions.BusinessException;
+import mx.qbits.tienda.api.model.exceptions.CustomException;
 
 @Service
 public class AnuncioServiceImpl implements AnuncioService{
 	private AnuncioMapper productoMapper;
 
-    // Constructor de la calse
+	/**
+     * Constructor de la clase.
+     *
+     * @param productoMapper
+     */
     public AnuncioServiceImpl(AnuncioMapper productoMapper) {
         this.productoMapper = productoMapper;
     }
 
-    // Cambiar
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public Anuncio dameAnuncio(int idAnuncio) throws BusinessException {
     	try{
             Anuncio producto = productoMapper.getById(idAnuncio);
             return producto;
         }catch(SQLException e) {
             throw new BusinessException(e);
-        }  
- 
+        }
+
     }
 
-
-    
-    public Anuncio actualizarCompra(int idAnuncio, int idComprador, int idCatalogoFormaPago) throws BusinessException {
-      
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Anuncio actualizarCompra(int idAnuncio, int idComprador, int idCatalogoFormaPago)
+    		throws BusinessException {
+    	Anuncio producto;
+    	Date fechaCompra;
+    	int regsActualizados;
         try {
-        	Anuncio producto = productoMapper.getById(idAnuncio);
-        	if (producto != null) {
-        		producto.setIdComprador(idComprador);
-        		producto.setActivo(false);
-                producto.setComprado(true);
-                producto.setIdCatalogoFormaPago(idCatalogoFormaPago);
-        		
-        		productoMapper.updateDatosCompra(producto);
-        	}
-        	return producto;
-            // Obtiene el id del comprador y lo cambia en el producto
-            // Cambia a true comprado y ¿desactiva producto?
-            // Actualiza ID comprad	or y desactivar producto son iguales.
+        	producto = productoMapper.getById(idAnuncio);
+        	if (producto == null)
+        		throw new CustomException(EnumMessage.NOT_FOUND, idAnuncio);
 
+        	producto.setIdComprador(idComprador);
+        	producto.setActivo(false);
+            producto.setComprado(true);
+            producto.setIdCatalogoFormaPago(idCatalogoFormaPago);
+            fechaCompra = new Date(System.currentTimeMillis());
+        	producto.setFechaCompra(fechaCompra);
+
+        	regsActualizados = productoMapper.updateDatosCompra(producto);
+        	if (regsActualizados < 1)
+        		throw new CustomException(EnumMessage.DATABASE, "Error al actualizar registro");
         } catch(SQLException e) {
-            throw new BusinessException(e);
-        }  
+        	throw new CustomException(EnumMessage.DATABASE, "Error localizando el producto por id");
+        }
+        return producto;
     }
-
-
 }
