52ac30bbbc7248e222ef69f045764120c525fce6
claudiaigs <30503254+claudiaigs@users.noreply.github.com>
2022-01-23 23:55:10 -0600

    documentacion

--- src/main/java/mx/qbits/tienda/api/service/MultimediaService.java
+++ src/main/java/mx/qbits/tienda/api/service/MultimediaService.java
@@ -2,7 +2,19 @@ package mx.qbits.tienda.api.service;
 
 import mx.qbits.tienda.api.model.exceptions.BusinessException;
 
-
+/**
+* Interface MultimediaService
+* @author 2 de Pastor
+* @version 1
+*/
 public interface MultimediaService{
+    
+    /**
+    * Metodo que salva un objeto en la tabla de Multimedia
+    * @param int idAnuncio
+    * @param String url
+    * @param String tipo
+    * @throw Business Exception
+    */
     int salvaMultimedia(int idAnuncio, String url, String tipo) throws BusinessException;
 }
