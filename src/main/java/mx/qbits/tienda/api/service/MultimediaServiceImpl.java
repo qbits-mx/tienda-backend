4f5973fb7adabe4a0122136e3ac254cee4cb2fe5
claudiaigs <30503254+claudiaigs@users.noreply.github.com>
2022-01-24 00:02:17 -0600

    documentación

--- src/main/java/mx/qbits/tienda/api/service/MultimediaServiceImpl.java
+++ src/main/java/mx/qbits/tienda/api/service/MultimediaServiceImpl.java
@@ -6,13 +6,31 @@ import org.springframework.stereotype.Service;
 import mx.qbits.tienda.api.mapper.MultimediaMapper;
 
 import mx.qbits.tienda.api.model.exceptions.BusinessException;
+
+/**
+* Implementacion MultimediaServiceImpl de la interface MultimediaService
+* @author 2 de Pastor
+* @version 1
+*/
 @Service
 public class MultimediaServiceImpl implements MultimediaService{
 	MultimediaMapper mapper;
 
+	/**
+	* Constructor por parametro
+	* @param MultimediaMapper mapper
+	*/
 	public MultimediaServiceImpl(MultimediaMapper mapper){
 		this.mapper = mapper;
 	}
+	
+	/**
+    	* Metodo que salva un objeto en la tabla de Multimedia
+   	* @param int idAnuncio
+    	* @param String url
+    	* @param String tipo
+    	* @throw Business Exception
+    	*/
 	@Override
 	public int salvaMultimedia(int idAnuncio, String url, String tipo) throws BusinessException{
 
